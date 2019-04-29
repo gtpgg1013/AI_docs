2b250c787fb98578dfd54797bd19cf22855140cf
gtpgg1013 <gtpgg1013@gmail.com>
2019-04-29 17:54:01 +0900

    04_29

---
+++ PowerShell/Mypowershell.ps1
@@ -0,0 +1,30 @@
+﻿Get-Command
+Get-Process
+Get-Help Get-Process # PowerShell 주석
+$env:PSModulePath
+
+Install-Module -Name Az -AllowClobber
+
+Import-Module Az.Accounts
+
+Get-ExecutionPolicy # PowerShell 스크립트 실행 정책 보여줌
+    # Restricted : 제한됨 (*.ps1 파일) : 강력하기 때문에 강한 보안 걸려있음
+    # Unrestricted : 실행 (보안상 위험) : 인증서 매칭
+
+Set-ExecutionPolicy Unrestricted # PowerShell 스크립트 실행 정책 수정
+# Set-ExecutionPolicy -ExecutionPolicy가 생략된 모습
+
+Add-AzAccount # 또는
+Connect-AzAccount
+
+Get-AzSubscription
+
+
+Set-AzureRmContext -SubscriptionId #'SubscriptionId'
+Select-AzSubscription -Subscription 80647935-3144-4ef2-8d55-587f415ef448 # 이 subscription에서 작업하도록 하겠다 <Subscription id>
+
+#-----------------------------------AzureRM 모듈 설치------------------------------------------#
+
+Install-Module -Name AzureRM -AllowClobber
+Import-Module AzureRM
+Connect-AzureRmAccount
---
+++ PowerShell/exercise0429.ps1
@@ -0,0 +1,13 @@
+﻿#실습
+Add-AzureRmAccount
+Get-AzureRmSubscription
+Set-AzureRmContext -SubscriptionId 'SubscriptionId'
+$rg = New-AzureRMResourceGroup -Name '20533E0204-LabRG' -Location 'AzureRegion'
+$vnet = New-AzureRmVirtualNetwork -ResourceGroupName $rg.ResourceGroupName -Name '20533E0204-vnet' -AddressPrefix '10.11.0.0/16' -Location $rg.Location
+Add-AzureRmVirtualNetworkSubnetConfig -Name 'Subnet1' -VirtualNetwork $vnet -AddressPrefix '10.11.0.0/24'
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
+
+$vnet = New-AzureRmVirtualNetwork -ResourceGroupName $rg.ResourceGroupName -Name '20533E0202-vnet' -AddressPrefix '10.1.0.0/16' -Location $rg.Location
+
+Add-AzureRmVirtualNetworkSubnetConfig -Name 'Subnet1' -VirtualNetwork $vnet -AddressPrefix '10.1.0.0/24'
+Add-AzureRmVirtualNetworkSubnetConfig -Name 'Subnet2' -VirtualNetwork $vnet -AddressPrefix '10.1.1.0/24'

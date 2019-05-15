dac8e7d4541ef95be567286e1123796964f2fc85
gtpgg1013 <gtpgg1013@gmail.com>
2019-05-15 17:48:51 +0900

    05_15_m2

--- rwork/titanic_ex.R
+++ rwork/titanic_ex.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(readxl)
+library(ggplot2)
 gender_submission<-read.csv("Data/titanic/gender_submission.csv")
 gender_submission %>% head()
 test<-read.csv("Data/titanic/test.csv")
@@ -10,5 +11,9 @@ str(test)
 str(train)
 View(test)
 View(train)
+
+test$Cabin
+
+table(test$Embarked)
 
 qplot(data=train,x=Age,y=PassengerId,color=Survived)
